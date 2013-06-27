462bc092913ddba076f30412f4f4f6f0f367f167
Lin He <he9lin@gmail.com>
2013-06-27 09:29:25 +0800

    Add NcdcRecordParser

--- src/main/java/com/heyook/hadoop/MaxTemperatureMapper.java
+++ src/main/java/com/heyook/hadoop/MaxTemperatureMapper.java
@@ -8,30 +8,18 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class MaxTemperatureMapper extends
     Mapper<LongWritable, Text, Text, IntWritable> {
 
+  private NcdcRecordParser parser = new NcdcRecordParser();
+
   @Override
   public void map(LongWritable key, Text value, Context context)
       throws IOException, InterruptedException {
 
-    String line = value.toString();
-    String year = line.substring(15, 19);
-    String temp = line.substring(87, 92);
-    if (!missing(temp)) {
+    parser.parse(value);
 
-      String normalizedTemp = normalize(temp);
-      int airTemperature = Integer.parseInt(normalizedTemp);
-      context.write(new Text(year), new IntWritable(airTemperature));
+    if (parser.isValidTemperature()) {
+      context.write(new Text(parser.getYear()),
+          new IntWritable(parser.getAirTemperature()));
     }
-  }
-
-  private boolean missing(String temp) {
-    return temp.equals("+9999");
-  }
-
-  private String normalize(String temp) {
-    if (temp.substring(0, 1).equals("+")) {
-      return temp.substring(1);
-    }
-    return temp;
   }
 
 }
